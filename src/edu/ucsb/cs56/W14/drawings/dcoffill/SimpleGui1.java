612e46707b393760cefa31496f36b5fbf2eaeb41
David Coffill <dcoffill@cs.ucsb.edu>
2014-02-13 12:16:29 -0800

    Added SimpleGui1.java and modified package name

---
+++ src/edu/ucsb/cs56/W14/drawings/dcoffill/SimpleGui1.java
@@ -0,0 +1,27 @@
+package edu.ucsb.cs56.w14.drawings.dcoffill;
+import javax.swing.*;
+
+/** SimpleGui1 comes from Head First Java 2nd Edition p. 355.
+      It illustrates a simple GUI with a Button that doesn't do anything yet.
+
+     @author Head First Java, 2nd Edition p. 355
+     @author P. Conrad (who only typed it in and added the Javadoc comments)
+     @author TODO: Add additional author here
+     @version CS56, Spring 2013, UCSB
+*/
+
+public class SimpleGui1 {
+
+    /** main method to fire up a JFrame on the screen
+          @param args not used
+    */
+
+    public static void main (String[] args) {
+	JFrame frame = new JFrame() ;
+	JButton button = new JButton("click me") ;
+	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
+	frame. getContentPane() . add(button) ;
+	frame. setSize(300,300) ;
+	frame. setVisible(true) ;
+    }
+}
