8239fe82359846233d554ae94469ea9bd6fce007
Jenny Vien <jennyvien@umail.ucsb.edu>
2014-02-16 02:30:58 -0800

    added updated version of SimpleGui1 to my src directory - done with Step 5

--- src/edu/ucsb/cs56/W14/drawings/jennyvien/SimpleGui1.java
+++ src/edu/ucsb/cs56/W14/drawings/jennyvien/SimpleGui1.java
@@ -7,7 +7,7 @@ import javax.swing.*;
      @author Head First Java, 2nd Edition p. 355
      @author P. Conrad (who only typed it in and added the Javadoc comments)
      @author Jenny Vien
-     @version CS56, Spring 2013, UCSB
+     @version CS56, Winter 2014, UCSB
 */
 
 public class SimpleGui1 {
