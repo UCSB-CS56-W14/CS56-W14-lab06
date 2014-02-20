45eaa7891d44d6be971ffbbe89f7ed5c190af652
Ariel Xin <Ariel@169-231-80-54.wireless.ucsb.edu>
2014-02-20 15:03:34 -0800

    fix the snowman

--- src/edu/ucsb/cs56/W14/drawings/xin/simple/PictureComponent.java
+++ src/edu/ucsb/cs56/W14/drawings/xin/simple/PictureComponent.java
@@ -16,7 +16,7 @@ import java.awt.geom.GeneralPath; // combinations of lines and curves
    A component that draws a Picture by Phill Conrad
    
    @author Phill Conrad (original drawing)
-   @author YOUR NAME HERE (fixed the snowmans's head)
+   @author Yue Xin (fixed the snowmans's head)
    @version for UCSB CS56, S13
 
    
@@ -79,7 +79,7 @@ public class PictureComponent extends JComponent
        
        Circle snowManBottomCircle = 
            new Circle
-           (
+	   (
              snowManCenterBottomX,
              snowManCenterBottomY - bottomRadius,
              bottomRadius
@@ -96,12 +96,13 @@ public class PictureComponent extends JComponent
        g2.draw(snowManMiddleCircle);
 
        // @@@ ADD CODE HERE TO DRAW THE TOP CIRCLE
-       
+       Circle snowManTopCircle = new Circle (snowManCenterBottomX, snowManCenterBottomY- bottomRadius*2-middleRadius*2-topRadius, topRadius);
+       g2.draw(snowManTopCircle);       
        
        // @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
        
        g2.drawString("A house and a snowman, by Phill Conrad", 20,20);
-       g2.drawString("Top of snowman added by YOUR NAME HERE", 20,40);
+       g2.drawString("Top of snowman added by Yue Xin", 20,40);
     }
 
         
