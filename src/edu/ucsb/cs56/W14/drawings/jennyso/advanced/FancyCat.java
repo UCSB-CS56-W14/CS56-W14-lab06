d60cb4f953e24c72cdc39de0b8041db0738c646a
Jenny So <jcso@umail.ucsb.edu>
2014-02-20 12:42:55 -0800

    created Cat and FancyCat in place of House, HouseWithWindows, and CoffeeCup

--- src/edu/ucsb/cs56/W14/drawings/jennyso/advanced/FancyCat.java
+++ src/edu/ucsb/cs56/W14/drawings/jennyso/advanced/FancyCat.java
@@ -20,47 +20,41 @@ import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
    @version for CS56, W11, UCSB, 02/23/2011
    
 */
-public class HouseWithWindows extends House implements Shape
+public class FancyCat extends Cat implements Shape
 {
     /**
      * Constructor for objects of class CoffeeCup
      */
-    public HouseWithWindows(double x, double y, double width, double height)
+    public FancyCat(double x, double y, double width, double height)
     {
-	// construct the basic house shell
-	super(x,y,width,height);
+	// construct the basic cat shell
+	super(x,y,width*.75,height*.75);
 
+	//modify cat head dimensions to accomodate hat
+	double headHeight = height*.75;
+	double headWidth = width*.75;
+	
 	// get the GeneralPath that we are going to append stuff to
 	GeneralPath gp = this.get();
-	
-	// Make three windows, spaced like this, where w=width/10.0;
-	// | +--+ +--+ +--+ |
-	// | |  | |  | |  | |
-	// | +--+ +--+ +--+ |
-	// |w 2w w 2w w w2 w|
-	//
-	// The top of window will be at y + 0.5*height and the
-	// height of the window is 0.25height;
 
-	double w = 0.10 * width;
-	double winTop = y + 0.5 * height;
-	double winHt =  0.25 * height;
+	double brimWidth = 0.6 * width;
+	double hatHeight = .25*height;
+	double hatWidth = 0.3*width;
 
-	Rectangle2D.Double win1 =
-	    new Rectangle2D.Double(x + w, winTop, 2.0 * w, winHt);
-	Rectangle2D.Double win2 =
-	    new Rectangle2D.Double(x + 4.0*w, winTop, 2.0 * w, winHt);
-	Rectangle2D.Double win3 =
-	    new Rectangle2D.Double(x + 7.0*w, winTop, 2.0 * w, winHt);
+	Line2D.Double brim =
+	    new Line2D.Double(x - brimWidth/2.0, y-headHeight, 
+			      x + brimWidth/2.0, y-headHeight);
+	Rectangle2D.Double hat =
+	    new Rectangle2D.Double(x-hatWidth/2.0, y-height, 
+				   hatWidth, hatHeight);
 	
 	// add the windows to the house
 	// Look up the meaning of the second parameter of append
 	// (Hint--is a method of "GeneralPath")
 
-        GeneralPath wholeHouse = this.get();
-        wholeHouse.append(win1, false);
-        wholeHouse.append(win2, false);
-        wholeHouse.append(win3, false); 
+        GeneralPath wholeCat = this.get();
+        wholeCat.append(brim, false);
+        wholeCat.append(hat, false);
     }
 
 }
