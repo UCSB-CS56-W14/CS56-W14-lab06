1b0897e3d3f06afe1aaf5865cd0123e69e1d20ea
Eric Palyan <aepalyan@umail.ucsb.edu>
2014-02-23 17:03:16 -0800

    finished custom classes

--- src/edu/ucsb/cs56/W14/drawings/aepalyan/advanced/Lollipop.java
+++ src/edu/ucsb/cs56/W14/drawings/aepalyan/advanced/Lollipop.java
@@ -7,6 +7,7 @@ import java.awt.Shape; // general class for shapes
 import java.awt.geom.Point2D; 
 import java.awt.geom.Line2D; 
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.Rectangle;
 import java.awt.geom.PathIterator;
 import java.awt.geom.AffineTransform;
@@ -15,61 +16,46 @@ import edu.ucsb.cs56.w14.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
 
 /**
-   A vector drawing of a face that implements
-   the Shape interface, and so can be drawn, as well as
-   rotated, scaled, etc.
-      
-   @author Eric Palyan 
-   @version for CS56, Winter 14, UCSB
-   
+  A vector drawing of a lollipop that implements
+  the Shape interface, and so can be drawn, as well as
+  rotated, scaled, etc.
+
+  @author Eric Palyan 
+  @version for CS56, Winter 14, UCSB
+
 */
-public class Face extends GeneralPathWrapper implements Shape
+public class Lollipop extends GeneralPathWrapper implements Shape
 {
-    /**
-       Constructor
+	/**
+	  Constructor
 
-       @param x x coord of lower left corner of house
-       @param y y coord of lower left corner of house
-       @param width width of the face
-       @param height height of the face
-     */
-    public Face(double x, double y, double width, double height)
-    {
-    
-        // Rather than having to scale at the end, we can just
-        // draw things the right way to begin with, using the
-        // x, y, width and height.   If you haven't already
-        // hard coded a particular drawing, this may be an easier
-        // way.
-	
-	double eyeLocationX = x*.75;
-	double eyeLocationY = y*.75;
-        
-        // Make the first story
-        Eclipse2D.Double face = new Eclipse2D.Double(x, y, width, height);
+	  @param x x coord of lower left corner of lollipop head
+	  @param y y coord of lower left corner of lollipop head
+	  @param width width of the lollipop head
+	  @param height height of the lolipop head
+	  */
+	public Lollipop(double x, double y, double width, double height)
+	{
 
-	// Make the eyes
-	Eclipse2D.Double leftEye = new Eclipse2D.Double(eyeLocationX, eyeLocationY,
+	    // Rather than having to scale at the end, we can just
+	    // draw things the right way to begin with, using the
+	    // x, y, width and height.   If you haven't already
+	    // hard coded a particular drawing, this may be an easier
+	    // way.
+	    
+	    
+	    // Make the candy head
+	    Ellipse2D.Double candy = new Ellipse2D.Double(x, y, width, height);
+	    
+            
+	    // make the lollipop handle (stick)
+	    Rectangle2D.Double handle = new Rectangle2D.Double(x+(width/2*.9), y+height, width*.1, height*.75);
 
-                          
-        // make the roof.   Remember that y goes DOWN the page,
-        // so we ADD to y to get a "lower" value on the screen
-        
-        Line2D.Double leftRoof = 
-            new Line2D.Double (x, y + roofHeight,
-                               x + width/2.0, y);
-                               
-        Line2D.Double rightRoof =
-            new Line2D.Double (x + width/2.0, y,
-                               x + width, y + roofHeight);
-
-        // put the whole house together
-       
-        GeneralPath wholeHouse = this.get();
-        wholeHouse.append(firstStory, false);
-        wholeHouse.append(leftRoof, false);
-        wholeHouse.append(rightRoof, false); 
-        
+	    // put the whole lollipop together
+	    GeneralPath Lollipop = this.get();
+	    Lollipop.append(candy, false);
+	    Lollipop.append(handle, false);
+	    
     }
-
+    
 }
