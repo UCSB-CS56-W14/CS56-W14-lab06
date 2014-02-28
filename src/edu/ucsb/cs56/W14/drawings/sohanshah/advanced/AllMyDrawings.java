472546e77ae4d8223f74430ec9abf55031ae064a
Sohan Shah <sohanshah@umail.ucsb.edu>
2014-02-27 22:36:51 -0800

    finished LongerHopscotch class and made 3 different drawings in AllMyDrawings class

--- src/edu/ucsb/cs56/W14/drawings/sohanshah/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/W14/drawings/sohanshah/advanced/AllMyDrawings.java
@@ -19,34 +19,32 @@ import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
 /**
  * A class with static methods for drawing various pictures
  * 
- * @author Phill Conrad 
- * @version for CS10, lab06, Spring 2009
+ * @author Sohan Shah 
+ * @version for CS56, lab06, Winter 2014
  */
 
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few 4-step hopscotches 
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
-	
-	Hopscotch h = new Hopscotch (300, 50, 50);
-	g2.draw(h);
-	// Make a black house that's half the size, 
+   	
+	Hopscotch h = new Hopscotch (150, 30, 20);
+	g2.setColor(Color.CYAN);g2.draw(h);
+	// Make a black hopscotch that's half the size, 
 	// and moved over 150 pixels in x direction
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape h1 = ShapeTransforms.scaledCopyOfLL(h,0.5,0.5);
+	h1 = ShapeTransforms.translatedCopyOf(h1,150,0);
+	g2.setColor(Color.BLACK); g2.draw(h1);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a hopscotch that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	h1 = ShapeTransforms.scaledCopyOfLL(h1,4,4);
+	h1 = ShapeTransforms.translatedCopyOf(h1,150,250);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -58,52 +56,38 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(h1); 
 	
-	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	LongerHopscotch lh1 = new LongerHopscotch(70,5,25);
+	LongerHopscotch lh2 = new LongerHopscotch(110,130,60);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(lh1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(lh2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("Translated and transformed hopscotch courts by Sohan Shah", 220,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Rotated hopscotch
      */
     public static void drawPicture2(Graphics2D g2) {
 
-	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
-	
-	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
-	
-	House h1 = new House(100,250,50,75);
+	Hopscotch h1 = new Hopscotch(100,150,75);
 	g2.setColor(Color.CYAN); g2.draw(h1);
 	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
 	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	h2 = ShapeTransforms.translatedCopyOf(h2,150,50);
 	g2.setColor(Color.BLACK); g2.draw(h2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
+	h2 = ShapeTransforms.scaledCopyOfLL(h2,2,2);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	
 	// We'll draw this with a thicker stroke
@@ -116,45 +100,37 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
 	
-	// Draw two houses with Windows
-	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); 
 
 	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	Shape h3 = ShapeTransforms.rotatedCopyOf(h2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(h3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A few hopscotch courts with one rotated by Sohan Shah", 20,20);
     }
   
-    /** Draw a different picture with a few houses and coffee cups
+    /** Picture with some longer hopscotch courts
      */
 
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("Let's Play Hopscotch by Sohan Shah", 20,20);
 
 	
-	// Draw some coffee cups.
+	// Draw some long hopscotches
 	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+       LongerHopscotch large = new LongerHopscotch(270,50,70);
+       LongerHopscotch small = new LongerHopscotch(40,50,30);
        
        g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
+       g2.setColor(Color.GREEN);   g2.draw(small);
        
        
     }
