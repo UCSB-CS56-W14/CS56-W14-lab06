355dfeb3014a404658d3124d5196df2b52a5cf57
jgee67 <jgee@umail.ucsb.edu>
2014-02-28 02:43:06 -0800

    Created and Tested Glass and FullGlass

--- src/edu/ucsb/cs56/W14/drawings/jgee/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/W14/drawings/jgee/advanced/AllMyDrawings.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs56.w14.drawings.pconrad.advanced;
+package edu.ucsb.cs56.w14.drawings.jgee.advanced;
 
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;  // single lines
@@ -26,12 +26,12 @@ import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few glasses 
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	House h1 = new House(100,250,50,75);
+	Glass h1 = new Glass(100,250,50,75);
 	g2.setColor(Color.CYAN); g2.draw(h1);
 	
 	// Make a black house that's half the size, 
@@ -60,8 +60,8 @@ public class AllMyDrawings
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	FullGlass hw1 = new FullGlass(50,350,40,75);
+	FullGlass hw2 = new FullGlass(200,350,200,100);
 	
 	g2.draw(hw1);
 	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
@@ -70,27 +70,27 @@ public class AllMyDrawings
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few glasses by Julian Gee", 20,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few glasses & full glasses
      */
     public static void drawPicture2(Graphics2D g2) {
 
 	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	Glass large = new Glass(100,50,225,150);
+	Glass smallCC = new Glass(20,50,40,30);
+	Glass tallSkinny = new Glass(20,150,20,40);
+	Glass shortFat = new Glass(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
 	g2.setColor(Color.GREEN);   g2.draw(smallCC);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
+	Glass h1 = new Glass(100,250,50,75);
 	g2.setColor(Color.CYAN); g2.draw(h1);
 	
 	// Make a black house that's half the size, 
@@ -118,8 +118,8 @@ public class AllMyDrawings
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	FullGlass hw1 = new FullGlass(50,350,40,75);
+	FullGlass hw2 = new FullGlass(200,350,200,100);
 	
 	g2.draw(hw1);
 	g2.setColor(new Color(0x8F00FF)); 
@@ -133,23 +133,23 @@ public class AllMyDrawings
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of glasses and full glasses by Julian Gee", 20,20);
     }
   
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few glasses and full glasses
      */
 
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of glasses by Julian Gee", 20,20);
 
 	
 	// Draw some coffee cups.
 	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+       Glass large = new Glass(100,50,225,150);
+       Glass smallCC = new Glass(20,50,40,30);
        
        g2.setColor(Color.RED);     g2.draw(large);
        g2.setColor(Color.GREEN);   g2.draw(smallCC);
