15b6c8b87cf0aece5e3cd9305c2152d86396c2e5
Khalid Dhanani <khaliddhanani@gmail.com>
2014-02-27 23:43:28 -0800

    Replaced pconrad drawings with my drawings

--- src/edu/ucsb/cs56/W14/drawings/khalid/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/W14/drawings/khalid/advanced/AllMyDrawings.java
@@ -26,7 +26,7 @@ import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few ice cream cones
      */
 
     public static void drawPicture1(Graphics2D g2) {
@@ -36,13 +36,54 @@ public class AllMyDrawings
 	
 
 	Shape c2 = ShapeTransforms.scaledCopyOfLL(c1,0.5,0.5);
+	c2 = ShapeTransforms.translatedCopyOf(c2,250,0);
+	g2.setColor(Color.BLACK); g2.draw(c2);
+	
+	c2 = ShapeTransforms.scaledCopyOfLL(c2,3,3);
+	c2 = ShapeTransforms.translatedCopyOf(c2,100,150);
+	
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	
+	Stroke orig=g2.getStroke();
+	g2.setStroke(thick);
+	g2.setColor(new Color(0x002FA7)); 
+	g2.draw(c2); 
+	
+	
+	SprinkledIceCreamCone sp1 = new SprinkledIceCreamCone(50,400,40);
+        SprinkledIceCreamCone sp2 = new SprinkledIceCreamCone(200,450,80);
+	
+	g2.draw(sp1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(sp2);
+	
+	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
+	
+	g2.setStroke(orig);
+	g2.setColor(Color.BLACK); 
+	g2.drawString("A few ice cream cones by Khalid Dhanani", 20,20);
+    }
+
+
+    /** Draw a picture with a few ice cream cones
+     */
+    public static void drawPicture2(Graphics2D g2) {
+
+	
+        IceCreamCone large = new IceCreamCone(330,360,90);
+        IceCreamCone small = new IceCreamCone(30,450,30);
+	
+	g2.setColor(Color.BLUE);    g2.draw(large);
+	g2.setColor(Color.MAGENTA); g2.draw(small);
+	
+	IceCreamCone c1 = new IceCreamCone(100,350,65);
+	g2.setColor(Color.CYAN); g2.draw(c1);
+	
+	Shape c2 = ShapeTransforms.scaledCopyOfLL(c1,0.5,0.5);
 	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
 	g2.setColor(Color.BLACK); g2.draw(c2);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	c2 = ShapeTransforms.scaledCopyOfLL(c2,4,4);
-	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
+	c2 = ShapeTransforms.scaledCopyOfLL(c2,2,2);
+	c2 = ShapeTransforms.translatedCopyOf(c2,350,50);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -56,101 +97,40 @@ public class AllMyDrawings
 	g2.setColor(new Color(0x002FA7)); 
 	g2.draw(c2); 
 	
-	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+        SprinkledIceCreamCone sp1 = new SprinkledIceCreamCone(50,350,25);
+	SprinkledIceCreamCone sp2 = new SprinkledIceCreamCone(370,500,55);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
-    }
-
-
-    /** Draw a picture with a few houses and coffee cups
-     */
-    public static void drawPicture2(Graphics2D g2) {
-
-	// Draw some coffee cups.
-	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
-	
-	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
-	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
-	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
-	
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
-	
-	// Draw two houses with Windows
-	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
+	g2.draw(sp1);
 	g2.setColor(new Color(0x8F00FF)); 
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second cone 45 degrees around its center.
+	Shape sp3 = ShapeTransforms.rotatedCopyOf(sp2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(sp3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("More ice cream cones by Khalid Dhanani", 20,20);
     }
   
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few ice cream cones
      */
 
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of ice cream cones by Khalid Dhanani", 20,20);
 
 	
-	// Draw some coffee cups.
-	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+	IceCreamCone large = new IceCreamCone(200,350,40);
+       IceCreamCone small = new IceCreamCone(120,250,30);
        
        g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
+       g2.setColor(Color.GREEN);   g2.draw(small);
        
        
     }
--- src/edu/ucsb/cs56/W14/drawings/khalid/advanced/SprinkledIceCreamCone.java
+++ src/edu/ucsb/cs56/W14/drawings/khalid/advanced/SprinkledIceCreamCone.java
@@ -17,15 +17,15 @@ import edu.ucsb.cs56.w14.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w14.drawings.utilities.GeneralPathWrapper;
 
 /**
-   A vector drawing of an ice cream cone that implements
-   the Shape interface, and so can be drawn, as well as
+   A vector drawing of an ice cream cone with sprinkles that implements
+   the Shape interface and extends IceCreamCone, and so can be drawn, as well as
    rotated, scaled, etc.
       
    @author Khalid Dhanani
    @version for CS56, Winter 11, UCSB
    
 */
-public class IceCreamCone extends GeneralPathWrapper implements Shape
+public class SprinkledIceCreamCone extends IceCreamCone implements Shape
 {
     /**
        Constructor
@@ -34,29 +34,30 @@ public class IceCreamCone extends GeneralPathWrapper implements Shape
        @param y y coord of bottom of cone
        @param r r of ice cream scoop
      */
-    public IceCreamCone(double x, double y, double r)
+    public SprinkledIceCreamCone(double x, double y, double r)
     {
-    
+	super(x,y,r);
+
+	GeneralPath gp = this.get();
         double coneHeight = 4 * r;
         
-        
-        Ellipse2D.Double iceCream = 
-            new Ellipse2D.Double(x - r, y - coneHeight - r,
-                          2 * r, 2 * r);
+        Line2D.Double sprinkle1 = 
+            new Line2D.Double(x - 0.2 * r, y - coneHeight - 0.2 * r,
+                          x - 0.3 * r, y - coneHeight - 0.3 * r);
                             
-        Line2D.Double leftCone = 
-            new Line2D.Double (x, y,
-                               x - r, y - coneHeight);
+        Line2D.Double sprinkle2 = 
+            new Line2D.Double (x + 0.7 * r, y - coneHeight - 0.6 * r,
+                               x + 0.8 * r, y - coneHeight - 0.5 * r);
                                
-        Line2D.Double rightCone =
-            new Line2D.Double (x, y,
-                               x + r, y - coneHeight);
+        Line2D.Double sprinkle3 =
+            new Line2D.Double (x + 0.3 * r, y - coneHeight + 0.3 * r,
+                               x + 0.4 * r, y - coneHeight + 0.5 * r);
 
        
         GeneralPath wholeCone = this.get();
-        wholeCone.append(leftCone, false);
-        wholeCone.append(rightCone, false);
-        wholeCone.append(iceCream, false); 
+        wholeCone.append(sprinkle1, false);
+        wholeCone.append(sprinkle2, false);
+        wholeCone.append(sprinkle3, false); 
         
     }
 
