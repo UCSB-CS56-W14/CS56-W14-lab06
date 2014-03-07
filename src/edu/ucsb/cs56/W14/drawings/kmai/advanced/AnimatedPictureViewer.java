57c126411bbe42279f9106e33977b870d577c1e2
kmai <kmai@umail.ucsb.edu>
2014-03-06 16:16:28 -0800

    Added a second star/thread

--- src/edu/ucsb/cs56/W14/drawings/kmai/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/W14/drawings/kmai/advanced/AnimatedPictureViewer.java
@@ -10,15 +10,19 @@ public class AnimatedPictureViewer{
     private StarOfDavid star = new StarOfDavid(0,0,100);
     
     Thread anim; 
+    Thread anim2;
     
-    private int x = 100;
-    private int y = 100;
+    private int x = 1;
+    private int y = 1;
 
-    private int x2 = 300;
-    private int y2 = 300;
+    private int x2 = 430;
+    private int y2 = 1;
     
     private int dx = 5;
     private int dy = 5;
+
+    private int dx2 = 5;
+    private int dy2 = 5;
 
     public static void main (String[] args) {
       new AnimatedPictureViewer().go();
@@ -35,11 +39,14 @@ public class AnimatedPictureViewer{
       frame.getContentPane().addMouseListener(new MouseAdapter() {
         public void mouseEntered(MouseEvent e){
           anim = new Animation();
-          anim.start();
+	  anim2 = new Animation2();
+	  anim.start();
+	  anim2.start();
         }
 
         public void mouseExited(MouseEvent e){        
           anim.interrupt();
+	  anim2.interrupt();
           while (anim.isAlive()){}
           anim = null;         
           panel.repaint();        
@@ -77,14 +84,12 @@ public class AnimatedPictureViewer{
           while (true) {
             // Bounce off the walls
 	      double random = Math.random();
-	      int base = 50;
+	      int base = 40;
 	      if (x >= 550) { dx = -5; }
 	      if (x <= 0) { dx = 5; }
 	      if (y >= 350) { dy = -5; }
 	      if (y <= 0) { dy = 5; }
 	      
-	      if(x>0 && x<150) { base = 20; }
-	      if(x>300 && x<450) { base = 20; }
 	      x += dx;               
 	      y += dy;
 	      panel.repaint();
@@ -92,14 +97,42 @@ public class AnimatedPictureViewer{
 	      Thread.sleep((int)random);
           }
         } catch(Exception ex) {
-          if (ex instanceof InterruptedException) {
-            // Do nothing - expected on mouseExited
+	    if (ex instanceof InterruptedException) {
+	      // Do nothing - expected on mouseExited
           } else {
-            ex.printStackTrace();
+	      ex.printStackTrace();
             System.exit(1);
           }
         }
       }
     }
-    
+
+  class Animation2 extends Thread {
+      public void run() {
+        try {
+          while (true) {
+            // Bounce off the walls
+	      double random = Math.random();
+	      int base = 50;
+	      if (x2 >= 550) { dx2 = -5; }
+	      if (x2 <= 0) { dx2 = 5; }
+	      if (y2 >= 350) { dy2 = -5; }
+	      if (y2 <= 0) { dy2 = 5; }
+	      
+	      x2 += dx2;               
+	      y2 += dy2;
+	      panel.repaint();
+	      random = random * base;
+	      Thread.sleep((int)random);
+          }
+        } catch(Exception ex) {
+	    if (ex instanceof InterruptedException) {
+	      // Do nothing - expected on mouseExited
+          } else {
+	      ex.printStackTrace();
+            System.exit(1);
+          }
+        }
+      }
+    }
 }
